04bb0e1d290f5d06e51b6c5449b34e686d639f2c
hansd99 <hans.dekker99@gmail.com>
2023-05-08 11:13:05 +0200

    create files for lib package

---
+++ README.md
@@ -0,0 +1,3 @@
+# lib package for team 19 (REMLA 2023)
+
+This is a version aware lib package on which the front-end depends.
---
+++ src/lib_remla19/utils.py
@@ -0,0 +1,6 @@
+class VersionUtil:
+    def __init__(self, version='0.0.0'):
+        self._version = version
+
+    def get_version(self):
+        return self._version
